c8e34878786c08cb807fb1b539fb054a0c1cecd4
subham-dhakal <subhamdhakal123@gmail.com>
2020-04-01 17:37:07 +0545

    initial commit

---
+++ classes and instances.py
@@ -0,0 +1,17 @@
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {} '.format(self.first, self.last)
+
+
+emp1 = Employee('Subham', 'dhakal', 50000)
+emp2= Employee('Ram', 'bahadur', 30000)
+
+print(emp1.fullname())
+print(Employee.fullname(emp2))
